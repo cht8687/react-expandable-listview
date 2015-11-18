c91988f8c4a0c034f852b6fffd7fc77267c11f4f
Haotian.Chang <cht8687@gmail.com>
2015-11-18 23:45:25 +1100

    we don't use utility.js now

--- src/lib/Utility.js
+++
@@ -1,9 +0,0 @@
-/* 
-  Function receive headerRef and return
-  according itemRef 
-*/
-
-export function returnListRef(headerRef) {
-	const results = headerRef.split('-');
-	return `ListItem-${results[1]}`
-}
