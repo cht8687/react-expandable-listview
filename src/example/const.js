89cbfe14c9afb3ce017409ac02a51e0eeeb9cb7f
Haotian.Chang <cht8687@gmail.com>
2015-12-01 21:33:47 +1100

    move Data to separate file

---
+++ src/example/const.js
@@ -0,0 +1,119 @@
+export const DATA = [
+  {
+    headerName : "ListA",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  }, {
+    headerName : "ListB",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListC",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListD",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListE",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListF",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    }]
+  },{
+    headerName : "ListG",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListH",
+    isOpened: true,
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    }]
+  }
+];
